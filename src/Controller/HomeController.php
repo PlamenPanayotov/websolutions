11ae525bceea997545c5318e528e3b38f77fe7d4
PlamenPanayotov <p.panayotov@mail.bg>
2020-01-15 22:11:20 +0200

    add userForm

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,4 +22,22 @@ class HomeController extends AbstractController
     {
         return $this->render('home/about.html.twig');
     }
+
+    /**
+     * @Route("/register", name="register")
+     */
+
+    public function register()
+    {
+        return $this->render('security/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
+    }
 }
