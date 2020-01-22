a53fc43906110bdb6d37b3c4c820fab8cfa9ac67
PlamenPanayotov <p.panayotov@mail.bg>
2020-01-22 12:18:37 +0200

    some view changes

--- src/Form/AboutPostType.php
+++ src/Form/AboutPostType.php
@@ -6,14 +6,16 @@ use App\Entity\AboutPost;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AboutPostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
         ;
     }
 
